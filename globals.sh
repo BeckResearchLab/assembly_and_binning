2e2dd58b22d998c4e3f0e2ce5cf687ce202b71e9
EC2 Default User <ec2-user@ip-10-0-0-133.us-west-2.compute.internal>
2016-12-16 23:12:48 +0000

    first commit from prototype

---
+++ globals.sh
@@ -0,0 +1,10 @@
+# provide pathes
+export CONCOCT=/work/software/CONCOCT
+export DESMAN=/work/software/DESMAN
+
+# go into conda environment
+source activate concoct_env
+
+# now global vars
+pe_files="data/HOW11/trim/HOW11.pe.qc.fq.gz data/LOW6/trim/LOW6.pe.qc.fq.gz data/LOW11/trim/LOW11.pe.qc.fq.gz data/HOW6/trim/HOW6.pe.qc.fq.gz data/HOW4/trim/HOW4.pe.qc.fq.gz data/LOW8/trim/LOW8.pe.qc.fq.gz data/LOW4/trim/LOW4.pe.qc.fq.gz data/HOW8/trim/HOW8.pe.qc.fq.gz data/HOW12/trim/HOW12.pe.qc.fq.gz data/LOW7/trim/LOW7.pe.qc.fq.gz data/LOW12/trim/LOW12.pe.qc.fq.gz data/HOW7/trim/HOW7.pe.qc.fq.gz data/LOW10/trim/LOW10.pe.qc.fq.gz data/HOW5/trim/HOW5.pe.qc.fq.gz data/LOW9/trim/LOW9.pe.qc.fq.gz data/LOW5/trim/LOW5.pe.qc.fq.gz data/HOW9/trim/HOW9.pe.qc.fq.gz data/HOW10/trim/HOW10.pe.qc.fq.gz"
+se_files="data/HOW11/trim/HOW11.se.qc.fq.gz data/LOW6/trim/LOW6.se.qc.fq.gz data/LOW11/trim/LOW11.se.qc.fq.gz data/HOW6/trim/HOW6.se.qc.fq.gz data/HOW4/trim/HOW4.se.qc.fq.gz data/LOW8/trim/LOW8.se.qc.fq.gz data/LOW4/trim/LOW4.se.qc.fq.gz data/HOW8/trim/HOW8.se.qc.fq.gz data/HOW12/trim/HOW12.se.qc.fq.gz data/LOW7/trim/LOW7.se.qc.fq.gz data/LOW12/trim/LOW12.se.qc.fq.gz data/HOW7/trim/HOW7.se.qc.fq.gz data/LOW10/trim/LOW10.se.qc.fq.gz data/HOW5/trim/HOW5.se.qc.fq.gz data/LOW9/trim/LOW9.se.qc.fq.gz data/LOW5/trim/LOW5.se.qc.fq.gz data/HOW9/trim/HOW9.se.qc.fq.gz data/HOW10/trim/HOW10.se.qc.fq.gz"
