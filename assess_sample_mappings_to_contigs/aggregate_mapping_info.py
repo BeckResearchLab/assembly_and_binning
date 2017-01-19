9c0120007d23c10d420287f03ff8c76d9697a732
Janet Matsen <janetmatsen@gmail.com>
2017-01-19 14:31:09 +0000

    prep of file tracking frac of reads mapped to contigs in bins and out of bins.
    - renamed "reads mapped" to "mapped reads"
    - keep track of reads that map to contigs in bins
    - merge on the total reads per sample (result from parsing. fastq.gz)
    - coarsen binning to 100 equally spaced bins

--- assess_sample_mappings_to_contigs/aggregate_mapping_info.py
+++ assess_sample_mappings_to_contigs/aggregate_mapping_info.py
@@ -21,9 +21,9 @@ def coarsen_size_distribution(df, bin_edges):
     sums = pd.DataFrame(df.groupby('hist bin')['# mapped reads'].sum())
     sums.reset_index(inplace=True)
     # clean it up
-    sums.rename(columns={'# mapped reads':'sum(mapped reads)'}, inplace=True)
-    sums['sum(mapped reads)'] = sums['sum(mapped reads)'].fillna(0)
-    sums['sum(mapped reads)'] = sums['sum(mapped reads)'].astype(int)
+    sums.rename(columns={'# mapped reads':'sum(reads mapped)'}, inplace=True)
+    sums['sum(reads mapped)'] = sums['sum(reads mapped)'].fillna(0)
+    sums['sum(reads mapped)'] = sums['sum(reads mapped)'].astype(int)
     sums['cryptic metagenome name'] = df['cryptic metagenome name'].iloc[0]
     return sums
 
@@ -31,25 +31,49 @@ def parse_idxstats_bin_and_append_results(idxstat_dir, bins, outpath):
     idxstat_files = os.listdir(idxstat_dir)
     idxstat_paths = [os.path.join(idxstat_dir, p) for p in idxstat_files]
 
+    # only has contigs that *do* map to bins
+    bin_contig_mappings = pd.read_csv('../abundances/bin_contig_mappings.tsv', sep='\t')
+    binned_contigs = bin_contig_mappings['contigName'].unique().tolist() # len = 304871
+    # bin_contig_mappings.columns = ['contigName', 'file', 'contigs', 'bin', 'Bin Id', 'bin_id']
+    bin_contig_mappings = bin_contig_mappings[['contigName', 'bin']]
+
     sample_info = pd.read_csv('../data/sample_info/sample_info.tsv', sep='\t')
     del sample_info['project']
     sample_translation = pd.read_csv('../data/sample_info/meta4_sample_names--cryptic_to_sample_number.tsv', sep='\t')
     sample_translation = sample_translation[['sample id', 'cryptic metagenome name']]
+    total_reads_per_sample = pd.read_csv('../data/sample_info/sample_read_counts.tsv', 
+                                         names=['cryptic metagenome name', 'total reads (in fastq)'], sep='\t')
+    total_reads_per_sample['cryptic metagenome name'] = total_reads_per_sample['cryptic metagenome name'].str.strip('.fastq.gz')
 
     files_completed = 0
     for i in idxstat_paths:
         df = parse_idxstat(i)
+        
+        df['binned contig'] = False
+        df.loc[df['contig'].isin(binned_contigs), 'binned contig'] = True
+
         df_coarsened = coarsen_size_distribution(df=df, bin_edges=bins)
+
+        binned_coarsened = coarsen_size_distribution(df=df[df['binned contig'] == True].copy(), bin_edges=bins)
+        binned_coarsened.rename(columns={'sum(reads mapped)':'sum(reads mapped to bins)'}, inplace=True)
+        df_coarsened = pd.merge(df_coarsened, binned_coarsened[['hist bin', 'sum(reads mapped to bins)']], how='outer')
+
+        unbinned_coarsened = coarsen_size_distribution(df=df[df['binned contig'] == False].copy(), bin_edges=bins)
+        unbinned_coarsened.rename(columns={'sum(reads mapped)':'sum(reads not mapped to bins)'}, inplace=True)
+        df_coarsened = pd.merge(df_coarsened, unbinned_coarsened[['hist bin', 'sum(reads not mapped to bins)']], how='outer')
+
         df_coarsened = pd.merge(df_coarsened, sample_translation)
         df_coarsened = pd.merge(df_coarsened, sample_info)
+        df_coarsened = pd.merge(df_coarsened, total_reads_per_sample)
+        assert df_coarsened.shape[0] > 0, "df_coarsened has no rows left"
 
         if files_completed == 0:
-            df_coarsened.to_csv(outpath, sep='\t')
+            df_coarsened.to_csv(outpath, sep='\t', index=False)
         #elif files_completed > 3:
         #    return
         else:
             # append to the already existing file
-            df_coarsened.to_csv(outpath, sep='\t', mode='a', header=False)
+            df_coarsened.to_csv(outpath, sep='\t', mode='a', header=False, index=False)
         
         files_completed += 1
         print('done with file # {}'.format(files_completed))
@@ -64,7 +88,12 @@ if __name__ == '__main__':
     # [int(n) for n in np.linspace(0, 100000, 11).tolist()]
     bin_edges = [int(n) for n in np.linspace(0, magnitude, n_bins).tolist()]
 
+    outdir = './results'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    outfile = os.path.join(outdir, 'results.tsv')
+
     parse_idxstats_bin_and_append_results(
         '/work/m4b_binning/assembly/map_reads/flagstat/idxstat_results/', 
-        bins=bin_edges, outpath='./results--all_contigs.tsv')
+        bins=bin_edges, outpath=outfile)
 
