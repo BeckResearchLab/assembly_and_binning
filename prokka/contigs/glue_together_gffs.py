6e2e5b928320a7d9003e219b3e18875b7c6eb190
Janet Matsen <janetmatsen@gmail.com>
2017-02-10 15:52:27 +0000

    script to glue together separate gff files found by glob search.
    tested:
    (py3) [ec2-user@ip-10-0-0-158 contigs]$ pwd
    /work/m4b_binning/assembly/prokka/contigs
    (py3) [ec2-user@ip-10-0-0-158 contigs]$ python glue_together_gffs.py contigs_longer_than_1500bp/
    
    Then counted the number of lines that started with k:
    - I am still waiting for one Prokka to run, but this is what I've got:
        contigs_longer_than_1500bp/contigs_longer_than_1500bp_group_2/contigs_longer_than_1500bp_02092017.gff:191228
        contigs_longer_than_1500bp/contigs_longer_than_1500bp_group_3/contigs_longer_than_1500bp_02102017.gff:185735
        contigs_longer_than_1500bp/contigs_longer_than_1500bp_group_4/contigs_longer_than_1500bp_02102017.gff:181463
        contigs_longer_than_1500bp/contigs_longer_than_1500bp_group_5/contigs_longer_than_1500bp_02092017.gff:174015
        (py3) [ec2-user@ip-10-0-0-158 contigs]$ grep -c "^k" contigs_longer_than_1500bp/contigs_longer_than_1500bp_gffs_concatenated.gff
        732441
    - checked:
        732441 - (191228 + 185735 + 181463 + 174015) = 0

---
+++ prokka/contigs/glue_together_gffs.py
@@ -0,0 +1,58 @@
+import argparse
+import glob
+import os
+import sys
+
+
+def prep_filename(gff_dir):
+    # strip off trailing '/'
+    gff_dir = gff_dir.rstrip('/')
+    return os.path.join(gff_dir, '{}_gffs_concatenated.gff'.format(gff_dir))    
+
+def combine_gffs(gff_file_list, filename):
+
+    with open(filename, 'w') as outfile:
+        file_num = 1
+        for f in gff_file_list:
+            print('get the good stuff from {}'.format(filename))
+            with open(f) as f1:
+                for line_num, line in enumerate(f1):        #keep the header from file1
+                    # The first line is `##gff-version 3`.  Keep that for the first file. 
+                    if (file_num == 1) and (line_num == 0):
+                        outfile.write(line)
+                    # The end of the file has the entire FASTA sequence glued on.  Remove it.
+                    elif '##FASTA' in line:
+                        break
+                    # Delete subsequent lines like `##sequence-region k141_461591 1 2140`
+                    elif (line_num > 0) and line.startswith('##'):
+                        continue
+                    # Don't include the FASTA sequences at the bottom. 
+                    outfile.write(line)
+                file_num += 1
+
+if __name__ == '__main__':
+    # expect python 3 to be used.  Source activate py3
+    assert sys.version_info > (3, 0), 'expected python 3; got {}'.format(sys.version)    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('gff_parent_folder', help='parent folder to get gffs from')
+    args = parser.parse_args()
+    
+    if args.gff_parent_folder is None:
+        print(args.print_help())
+    else:
+        print('gff parent folder: {}'.format(args.gff_parent_folder))
+    
+    gff_file_list = []
+    search_path = args.gff_parent_folder + '/**/*.gff'
+    print('look for all gff files at paths like {}'.format(search_path))
+    for filename in glob.glob(search_path):
+        gff_file_list.append(filename)
+    print('found {} gff files: \n{}'.format(len(gff_file_list), gff_file_list))
+
+    filename = prep_filename(args.gff_parent_folder)
+    print('save concatenated gffs to {}'.format(filename))
+    
+    # combine the found gff files
+    combine_gffs(gff_file_list, filename)
+
